d5f52d71651dfdcc74b08416634d9adefc712e1f
Harold Treen <haroldtreen@gmail.com>
2012-06-03 10:14:45 -0400

    UI Progress

--- gen/com/onedatapoint/R.java
+++ gen/com/onedatapoint/R.java
@@ -16,18 +16,19 @@ public final class R {
     public static final class id {
         public static final int SliderActivityButton=0x7f050001;
         public static final int TextActivityButton=0x7f050000;
-        public static final int cancelButton=0x7f050004;
-        public static final int childFrame=0x7f050005;
+        public static final int cancelButton=0x7f050005;
+        public static final int childFrame=0x7f050006;
         public static final int editText1=0x7f050007;
-        public static final int parentLayout=0x7f050002;
-        public static final int saveButton=0x7f050003;
-        public static final int textView1=0x7f050006;
+        public static final int parentLayout=0x7f050003;
+        public static final int saveButton=0x7f050004;
+        public static final int textView1=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
-        public static final int parentlayout=0x7f030001;
-        public static final int sliderlayout=0x7f030002;
-        public static final int textlayout=0x7f030003;
+        public static final int medview=0x7f030001;
+        public static final int parentlayout=0x7f030002;
+        public static final int sliderlayout=0x7f030003;
+        public static final int textlayout=0x7f030004;
     }
     public static final class string {
         public static final int CancelButton=0x7f040002;
